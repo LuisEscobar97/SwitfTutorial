2282825a1e2f733ff45b8589df126d78517d858e
Luis Escobar <luisitoo366@gmail.com>
2022-03-06 21:25:20 -0600

    first commit

---
+++ TutorialSwift.swiftpm/variables.playground/Contents.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+let maximunNumberOfLogging = 3
+
+var currenTLoginAttempt = 0
+
+var welcomeMessage : String
+
+welcomeMessage="Hola que tal perros"
+
+var red, green, blue : Double
+
+print("el numero de veces \(maximunNumberOfLogging)")
+
+let f1: Float = 3.1415987466
+let d1: Double = 3.1415987466
+
+let dosMil : UInt16 = 2000
+let one : UInt8 = 1
+let dosMilUno = dosMil + UInt16(one)
+
+//alias que hace referencia a un tipo de dato
+typealias AudioSample = UInt16
+var Maxamplitud : AudioSample
+
